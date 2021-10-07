a897c52c1b723652ced807e0c84e760b0e04eb24
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-10-07 16:41:24 +0100

    handleChange & addItem passed as function as props (InputArea Component) still figuring out how to pass type and value in InputArea.jsx

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-function InputArea() {
+function InputArea(props) {
   return (
     <div className="form">
-      <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
+      <input
+        onChange={() => {
+          props.writeItem;
+        }}
+        // type="text"
+        // value={inputText}
+      />
+      <button
+        onClick={() => {
+          props.submitItem;
+        }}
+      >
         <span>Add</span>
       </button>
     </div>
