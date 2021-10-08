40a4c4a06e55af6b117722460dbb20636c2de3cb
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-10-08 15:43:12 +0100

    <InputArea /> :  Form is a separate component
    problems uploading from codesandbox

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,12 @@ import ToDoItem from "./ToDoItem";
 import InputArea from "./InputArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  function addItem() {
+  function addItem(inputText) {
     setItems((prevItems) => {
       return [...prevItems, inputText];
     });
-    setInputText("");
   }
 
   function deleteItem(id) {
@@ -31,8 +24,7 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <InputArea writeItem={handleChange} submitItem={addItem} />
-
+      <InputArea onAdd={addItem} />
       <div>
         <ul>
           {items.map((todoItem, index) => (
